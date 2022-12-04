4030bc4243c455d666e7ef7772d56ab827559b3c
niels.jautelat <niels.jautelat@stud.uni-goettingen.de>
2022-12-04 02:20:08 +0100

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+# bot.py
+import os
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.command(name='boop', help='boop')
+async def boop(ctx):
+    response = 'boop'
+    await ctx.send(response)
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
