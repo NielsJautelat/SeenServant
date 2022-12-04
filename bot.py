23d21bf7985909b891c9b25ce50d2b4c2cd4198e
niels.jautelat <niels.jautelat@stud.uni-goettingen.de>
2022-12-04 19:26:20 +0100

    Comments all around

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
 
+#A variable to help break a recursive loop
 loopbreak = False
-
 
 #Loading the .env file, containing your personal and secret Discord Token
 load_dotenv()
@@ -20,21 +20,12 @@ intents = discord.Intents.all()
 #This initializes the bot and defines the way to interact with the bot as '!command'
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
-
-
-
 #When it is ready to receive commands it will say that it is ready
 @bot.event
 async def on_ready():
     print("Bot is ready")
 
-
-
-
-
-
-#This command will make the bot join the voice channel
+#This command will make the bot join the voice channel that you are currently in
 @bot.command(name='join', help='The Bot joines the voice channel you are currently in')
 async def join(ctx):
     channel = ctx.message.author.voice.channel
@@ -46,12 +37,8 @@ async def leave(ctx):
     server = ctx.message.guild.voice_client
     await server.disconnect()
 
-
-
-
-
-#This command will make the bot play a specific local file
-@bot.command()
+#This command will make the bot play a specific local mp3 file, based on the relative path to the file
+@bot.command(name='play', help='The Bot plays a specific mp3 file \'!play song.mp3\'')
 async def play(ctx, file):
     channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild=ctx.guild)
@@ -68,30 +55,26 @@ async def play(ctx, file):
     file_name = file.rsplit("-", 2)
     await ctx.send(f"Playing: {file_name[0]}")
 
-
-
-
-
-@bot.command()
+#These commands make the Bot play a random rotation of mp3 files from specific folder/directory
+@bot.command(name='BattleMusic', help='Plays a random rotation of mp3 files in your BattleMusic folder/directory')
 async def BattleMusic(ctx):
     stoploop(ctx)
     await playcollection(ctx, 'BattleMusic')
-@bot.command()
+@bot.command(name='CalmMusic', help='Plays a random rotation of mp3 files in your CalmMusic folder/directory')
 async def CalmMusic(ctx):
     stoploop(ctx)
     await playcollection(ctx, 'CalmMusic')
-@bot.command()
+@bot.command(name='SuspensefulMusic', help='Plays a random rotation of mp3 files in your SuspensefulMusic folder/directory')
 async def SuspensefulMusic(ctx):
     stoploop(ctx)
     await playcollection(ctx, 'SuspensefulMusic')
-@bot.command()
+@bot.command(name='TavernMusic', help='Plays a random rotation of mp3 files in your TavernMusic folder/directory')
 async def TavernMusic(ctx):
     stoploop(ctx)
     await playcollection(ctx, 'TavernMusic')
 
 
-#This command will make the bot play a collection of files randomly ad nauseam 
-@bot.command()
+#This function will make the bot play a collection of mp3 files from a specific folder/directory randomly ad nauseam
 async def playcollection(ctx, folder):
     channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild=ctx.guild)
@@ -136,15 +119,15 @@ async def playcollection(ctx, folder):
         except:
             print("Error")
 
+#This function will stop the playlist
 def stoploop(ctx):
     voice_client = ctx.message.guild.voice_client
     if voice_client and voice_client.is_playing():
         global loopbreak
         loopbreak = True
         voice_client.stop()
-    
 
-
+#This command pauses the song
 @bot.command(name='pause', help='This command pauses the song')
 async def pause(ctx):
     voice_client = ctx.message.guild.voice_client
@@ -153,6 +136,7 @@ async def pause(ctx):
     else:
         await ctx.send("The bot is not playing anything at the moment.")
     
+#This command resumes the paused song
 @bot.command(name='resume', help='Resumes the song')
 async def resume(ctx):
     voice_client = ctx.message.guild.voice_client
@@ -161,6 +145,7 @@ async def resume(ctx):
     else:
         await ctx.send("The bot was not playing anything before this.")
 
+#This command skips the current song
 @bot.command(name='skip', help='Stops the song')
 async def skip(ctx):
     voice_client = ctx.message.guild.voice_client
@@ -169,15 +154,13 @@ async def skip(ctx):
     else:
         await ctx.send("The bot is not playing anything at the moment.")
 
+#This command stops the playlist
 @bot.command(name='stop', help='Stops the song')
 async def stop(ctx):
     stoploop(ctx)
 
-
-
-
-
-@bot.command(name='download')
+#This hidden command is there to download music from YouTube onto your computer so that you can put the mp3 files into the respective folders/directories
+@bot.command(hidden=True)
 async def download(ctx, url):
     await ctx.send("Getting your music")
 
@@ -195,17 +178,11 @@ async def download(ctx, url):
 
     await ctx.send("Finished Download")
 
-
-
-
-
+#boop
 @bot.command(name='boop', help='boop')
 async def boop(ctx):
     response = 'boop'
     await ctx.send(response)
 
-
-
-
-
+#And finally we are running the bot with this command
 bot.run(TOKEN)
